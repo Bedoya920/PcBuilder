0d340ae784afa6e89065d6735b851153f8e8bab8
Samuel Bedoya <samy040902@gmail.com>
2025-04-19 14:45:17 -0500

    Falta la mecánica de disparo para ambos jugadores y temporizador

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,30 +3,30 @@ using UnityEngine.InputSystem;
 
 public class Shooter : MonoBehaviour
 {
-    public Transform pivot; // Transform desde donde se dispararán las balas
+    //Este script es de prueba, solo es para probar la función del disparo. Eliminar despúes de usar
+
+    public Transform pivot; 
     private InputAction fireAction;
     public int ude;
 
     private void Awake()
     {
-        // Crea una nueva acción de entrada para el clic izquierdo del mouse
         fireAction = new InputAction("Fire", binding: "<Mouse>/leftButton");
-        fireAction.performed += ctx => Shoot(); // Suscribirse al evento de acción
+        fireAction.performed += ctx => Shoot(); 
     }
 
     private void OnEnable()
     {
-        fireAction.Enable(); // Habilitar la acción
+        fireAction.Enable(); 
     }
 
     private void OnDisable()
     {
-        fireAction.Disable(); // Deshabilitar la acción
+        fireAction.Disable(); 
     }
 
     private void Shoot()
     {
-       
         BulletPool.Instance.GetBullet(pivot, ude); 
     }
 }
